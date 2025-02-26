cdbe46d4401b587a5e2cd3bc1d94dd3a37b41afd
Samuel Verbrugge <sverbrug@terpmail.umd.edu>
2025-02-26 19:47:12 +0000

    Bitwise-Ex8.cpp

--- Bitwise-ex8.cpp
+++ Bitwise-ex8.cpp
@@ -1,1 +1,12 @@
 // Your code here
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main() {
+    bitset<8> b{0b01110010};
+        cout << b << " (initial value)\n";
+        b = ~b;
+        cout << b << " (final value)\n";
+    return 0;
+}
