204d4e38e6de62e4963dd22196b53579c41d9def
Samuel Verbrugge <sverbrug@terpmail.umd.edu>
2025-02-26 18:47:25 +0000

    UserInputFactorial-Ex5.cpp

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,18 @@
 // Add your code here
+#include <iostream>
+using namespace std;
+
+int fact (int num) {
+    int factorial=1;
+    for (int n=num; n>1; n--) {
+        factorial = factorial * n;
+    }
+    return factorial;
+}
+
+int main () {
+    int input;
+    cout << "Please enter an integer value: ";
+    cin >> input;
+    cout << "The value you entered is " << input << " and its factorial is " << fact(input) << "\n";
+}
