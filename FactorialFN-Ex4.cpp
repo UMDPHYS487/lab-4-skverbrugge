21f90ef92087146fede0161aaf722bde73530f5a
Samuel Verbrugge <sverbrug@terpmail.umd.edu>
2025-02-26 18:40:38 +0000

    FactorialFn-Ex4.cpp

--- FactorialFN-Ex4.cpp
+++ FactorialFN-Ex4.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
 int fact (int num) {
     int factorial=1;
-    while 
+    for (int n=num; n>1; n--) {
+        factorial = factorial * n;
+    }
+    return factorial;
 }
 
 int main () {
-
+    for (int n=1; n<11; n++) {
+        cout << "Factorial of " << n << " is " << fact(n) << "\n";
+    }
 }
