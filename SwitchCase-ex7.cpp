c6887662bc633ff2875e79813e52d7b635ea4456
Samuel Verbrugge <sverbrug@terpmail.umd.edu>
2025-02-26 19:24:53 +0000

    SwitchCase-Ex7.cpp

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,27 @@
 // add your code here
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input;
+    cout << "Please enter a number: ";
+    cin >> input;
+    switch (input % 2) {
+        case 0:
+            if (input > 10) {
+                cout << "hahaha\n";
+                break;
+            }
+            else {
+                cout << input * input << "\n";
+            }
+            break;
+        case 1:
+            cout << input / 2.0 << "\n";
+            break;
+        default:
+            cout << "hahaha\n";
+            break;
+    }
+    return 0;
+}
